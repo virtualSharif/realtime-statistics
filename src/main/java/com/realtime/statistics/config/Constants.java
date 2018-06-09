16dc8bbb80fcfa4eddd1f7a74ce6478cceb7377a
sharif.malik <malik_sharif@ymail.com>
2018-06-10 01:08:00 +0200

    introduce constants instead of hard coded values

---
+++ src/main/java/com/realtime/statistics/config/Constants.java
@@ -0,0 +1,13 @@
+package com.realtime.statistics.config;
+
+import java.text.DecimalFormat;
+
+public class Constants {
+
+    public static final int LAST_INTERESTED_INTERVAL_IN_SECONDS = 60;
+
+    public static final DecimalFormat DECIMAL_FORMAT = new DecimalFormat("#.##");
+
+    public static final Integer INTERESTED_INTERVAL_IN_MILLISECONDS = 60000;
+
+}
